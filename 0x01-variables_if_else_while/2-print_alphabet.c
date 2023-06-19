3eb3140843fd66d2138f05191a86bb35a35b11b3
MaryamEissa2 <maryameissa82003@gamil.com>
2023-06-19 16:49:53 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphapetic in lower cas
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
