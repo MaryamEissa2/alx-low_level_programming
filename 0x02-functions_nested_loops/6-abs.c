c5a008f54cd2c59e5d888f890f7629e7d1b2d588
MaryamEissa2 <maryameissa82003@gamil.com>
2023-06-30 04:55:43 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs -  computes the absolute value of an integer
+ * @r: pramater is integer
+ * Return: integer always
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
