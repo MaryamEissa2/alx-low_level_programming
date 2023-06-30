5983ec1b453e9dd55e46dac6a2c696b96a5a4f63
MaryamEissa2 <maryameissa82003@gamil.com>
2023-06-30 07:34:35 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d, \n", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
@@ -25,7 +25,7 @@ void print_to_98(int n)
 			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
-				printf("%d, \n", j);
+				printf("%d\n", j);
 		}
 	}
 }
