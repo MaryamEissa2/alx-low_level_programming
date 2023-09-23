5dc6a25e701242a48bf319d1a9c7526ed416bdbe
MaryamEissa2 <maryameissa82003@gmail.com>
2023-09-23 10:23:28 -0700

    task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 
 	return (0);
 }
